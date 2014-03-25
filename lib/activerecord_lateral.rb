6df0eb4ee69bfcc07d7a391d7c0ffec30a2974f9
Pavel Rosputko <pavel.rosputko@gmail.com>
2014-03-25 21:46:46 +0400

    Add support for has one. Trigger functionality by extra :lateral key on association.
    For exmample:
    has_one :last_comment, -> { order(created_at: :asc) }, class_name:
    Comment, lateral: true

--- lib/activerecord_lateral.rb
+++ lib/activerecord_lateral.rb
@@ -1,25 +1,36 @@
-module ActiveRecordLateral
-  module CollectionAssociationExt
-    def build_scope
-      super.order(preload_scope.values[:order] || reflection_scope.values[:order]).
-        limit(preload_scope.values[:limit] || reflection_scope.values[:limit])
-    end
+ActiveRecord::Associations::Builder::Association.valid_options << :lateral
 
-    def records_for(ids)
-      if preload_scope.values[:limit] || reflection_scope.values[:limit]
+ActiveRecord::Associations::Preloader::Association.class_eval do
+  def records_for(ids)
+    if reflection.options[:lateral]
+      relation = scope.where(association_key.eq(Arel.sql('ids.id')))
 
-        relation = scope.where(association_key.eq(Arel.sql('ids.id')))
-
-        # TODO escape or VALUES
-        klass.unscoped.
-          from("(SELECT unnest(ARRAY[#{ids.join(' ,')}]) id) ids, LATERAL (#{relation.to_sql}) #{klass.table_name}")
-      else
-        scope.where(association_key.in(ids))
-      end
+      # TODO escape or VALUES
+      klass.unscoped.
+        from("(SELECT unnest(ARRAY[#{ids.join(' ,')}]) id) ids, LATERAL (#{relation.to_sql}) #{klass.table_name}")
+    else
+      scope.where(association_key.in(ids))
     end
   end
 end
 
 ActiveRecord::Associations::Preloader::CollectionAssociation.class_eval do
-  include ActiveRecordLateral::CollectionAssociationExt
+  def build_scope
+    if reflection.options[:lateral]
+      super.order(preload_scope.values[:order] || reflection_scope.values[:order]).
+        limit(preload_scope.values[:limit] || reflection_scope.values[:limit])
+    else
+      super.order(preload_scope.values[:order] || reflection_scope.values[:order])
+    end
+  end
+end
+
+ActiveRecord::Associations::Preloader::HasOne.class_eval do
+  def build_scope
+    if reflection.options[:lateral]
+      super.order(preload_scope.values[:order] || reflection_scope.values[:order]).limit(1)
+    else
+      super.order(preload_scope.values[:order] || reflection_scope.values[:order])
+    end
+  end
 end
